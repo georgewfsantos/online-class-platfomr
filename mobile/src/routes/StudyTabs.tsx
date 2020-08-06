7265a64ae9ade0cde98f419d60c13ebf8f8b5a65
George W F dos Santos <gweverton21@hotmail.com>
2020-08-06 20:51:41 -0300

    style tab navigation and its routes

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -45,8 +45,14 @@ const StudyTabs: React.FC = () => {
         component={TeacherList}
         options={{
           tabBarLabel: "Proffys",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-easel" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? "#8257e5" : color}
+              />
+            );
           },
         }}
       />
@@ -55,8 +61,14 @@ const StudyTabs: React.FC = () => {
         component={Favorites}
         options={{
           tabBarLabel: "Favoritos",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-heart" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? "#8257e5" : color}
+              />
+            );
           },
         }}
       />
